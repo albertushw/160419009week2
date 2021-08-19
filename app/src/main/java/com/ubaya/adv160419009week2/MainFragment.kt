528f62c6383bea3a93eb378f5c2847267cfdbbfd
acer <s160419009@student.ubaya.ac.id>
2021-08-19 12:53:17 +0700

    init commit

---
+++ app/src/main/java/com/ubaya/adv160419009week2/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.ubaya.adv160419009week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MainFragment : Fragment() {
+
+    //hapus semua karena pake navigation component
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
